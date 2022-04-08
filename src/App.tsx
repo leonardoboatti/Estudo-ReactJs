ced426c570b982c3cda8beacf56674096c4e8f23
Leonardo Boatti <leonardo.boatti@yakult.com.br>
2022-04-08 15:18:43 -0300

    estudo react.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Header } from './components/Header';
+import { Photo } from './components/Photo';
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header title="Essse é um props"/>
+      <Header title='Outro texto'/>
+
+      Olá Mundo
+
+      <Photo legend="Google">
+        <img src="https://www.google.com.br/google.jpg" alt=""/>
+      </Photo>
     </div>
   );
 }
